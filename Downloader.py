328fd872be154c6e1cfaeec20a8c9f391950f76b
milosevich <milos.milosevich@gmail.com>
2019-12-19 12:00:20 +0100

    modified downloader

--- Downloader.py
+++ Downloader.py
@@ -8,14 +8,22 @@ def download_video(yt):
 def download_playlist(url_link):
     pl = Playlist(url_link)
     n=1
+    succes = 0
     pl.populate_video_urls()
     urls = pl.video_urls
     length = (len(pl.video_urls))
     for url in urls:
         print('------------------------------------')
         print(f'Video {str(n)} out of {str(length)}')
-        download_video (url)
-        n=n+1  
+        try:
+            download_video (url)
+            n = n + 1
+            succes = succes + 1
+        except KeyError as e:
+            print ('I got a KeyError - reason "%s"' % str(e))
+            n = n + 1
+            continue
+    print(f'Finished downloading {str(succes)} of {str(length)} videos.')
     
 def main():
     url_link = input('Enter URL: ')
@@ -23,7 +31,7 @@ def main():
         download_playlist(url_link)
     else:
         download_video(url_link)
-    print('Finished downloading.')
+    
     input("Press enter to close program")
 
 main()
