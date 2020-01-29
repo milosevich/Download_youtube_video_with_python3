14100e0126f88d476df7d44416b3ec4162de1b9b
milosevich <milos.milosevich@gmail.com>
2020-01-29 15:20:54 +0100

    best download

--- py_yt_download_new.py
+++ py_yt_download_new.py
@@ -3,6 +3,6 @@ import youtube_dl
 import urllib
 import shutil
 url_link = input('Enter URL: ')
-ydl_opts = {'format': 'bestvideo[ext=mp4]'}
+ydl_opts = {'format': 'best[ext=mp4]'}
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     ydl.download([url_link])
